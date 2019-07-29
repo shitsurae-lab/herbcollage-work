db795116cd30ab39ef56f19c86efdd3f0ebfa48a
Toshiyuki Kurashima <mahalo.morganite33@gmail.com>
2019-07-29 23:10:48 +0900

    let stand

--- header.php
+++ header.php
@@ -34,28 +34,28 @@ $container = get_theme_mod('understrap_container_type');
 			<nav class="navbar navbar-expand-md p-navbarContent navbar-dark bg-primary">
 
 				<?php if ('container' == $container) : ?>
-					<div class="container">
+				<div class="container">
 					<?php endif; ?>
 
 					<!-- Your site title as branding in the menu -->
 					<?php if (!has_custom_logo()) {
-						?>
+    ?>
 
-						<?php if (is_front_page() && is_home()) : ?>
+					<?php if (is_front_page() && is_home()) : ?>
 
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name'); ?></a></h1>
+					<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name'); ?></a></h1>
 
-						<?php else : ?>
+					<?php else : ?>
 
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name'); ?></a>
+					<a class="navbar-brand" rel="home" href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" itemprop="url"><?php bloginfo('name'); ?></a>
 
-						<?php endif; ?>
+					<?php endif; ?>
 
 
 					<?php
-					} else {
-						the_custom_logo();
-					} ?>
+} else {
+        the_custom_logo();
+    } ?>
 					<!-- end custom logo -->
 
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'understrap'); ?>">
@@ -64,19 +64,19 @@ $container = get_theme_mod('understrap_container_type');
 
 					<!-- The WordPress Menu goes here -->
 					<?php wp_nav_menu(
-						array(
-							'theme_location'  => 'primary',
-							'container_class' => 'collapse navbar-collapse',
-							'container_id'    => 'navbarNavDropdown',
-							'menu_class'      => 'navbar-nav ml-auto p-globalNav__menu',
-							'fallback_cb'     => '',
-							'menu_id'         => 'main-menu',
-							'depth'           => 2,
-							'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-						)
-					); ?>
+                        array(
+                            'theme_location'  => 'primary',
+                            'container_class' => 'collapse navbar-collapse',
+                            'container_id'    => 'navbarNavDropdown',
+                            'menu_class'      => 'navbar-nav ml-auto p-globalNav__menu',
+                            'fallback_cb'     => '',
+                            'menu_id'         => 'main-menu',
+                            'depth'           => 2,
+                            'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+                        )
+                    ); ?>
 					<?php if ('container' == $container) : ?>
-					</div><!-- .container -->
+				</div><!-- .container -->
 				<?php endif; ?>
 
 			</nav><!-- .site-navigation -->
